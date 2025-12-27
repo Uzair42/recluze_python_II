c4f3c00e818a9273984bbabe2d88194918dc05ab
uzair42 <muhammaduzairali48@gmail.com>
2025-12-27 23:00:36 +0500

    logiging

---
+++ logging/log1.py
@@ -0,0 +1,24 @@
+import logging 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+# Create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# Create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Add formatter to ch
+ch.setFormatter(formatter)
+
+# Add ch to logger
+logger.addHandler(ch)
+def log_messages():
+    logger.debug("This is a debug message")
+    logger.info("This is an info message")
+    logger.warning("This is a warning message")
+    logger.error("This is an error message")
+    logger.critical("This is a critical message")
+log_messages()
